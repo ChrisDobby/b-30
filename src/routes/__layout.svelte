883194c0cc5f362b477f2ae2f24283431b7ff824
Chris Dobson <chrisdobby.dev@gmail.com>
2021-11-14 12:55:24 +0000

    initial app

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+    import IconButton from "@smui/icon-button";
+    import "../app.scss";
+</script>
+
+<TopAppBar variant="static" color={"primary"}>
+    <Row>
+        <Section>
+            <IconButton class="material-icons">menu</IconButton>
+            <Title>Breaking 30</Title>
+        </Section>
+    </Row>
+</TopAppBar>
+<main>
+    <slot />
+</main>
+
+<style>
+    main {
+        padding: 16px;
+        height: 100%;
+        flex-grow: 1;
+        overflow-y: auto;
+    }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<div class="coming-soon">
+    <h4>Coming soon!</h4>
+</div>
+
+<style>
+    .coming-soon {
+        text-align: center;
+    }
+</style>
