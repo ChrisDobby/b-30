d5596786284a6f0d0d734f223cfcc4f1396a3352
Chris Dobson <chrisdobby.dev@gmail.com>
2021-11-14 16:10:36 +0000

    add a footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
     import IconButton from "@smui/icon-button";
+    import Button from "@smui/button";
     import "../app.scss";
 </script>
 
@@ -15,6 +16,15 @@
 <main>
     <slot />
 </main>
+<footer>
+    <p>
+        In April 2022 I am running a half marathon for charity. If you find this app useful please consider sponsoring
+        me.
+        <Button target="_blank" href="https://www.justgiving.com/chrisdobby" outlined color={"primary"}
+            >Sponsor me!</Button
+        >
+    </p>
+</footer>
 
 <style>
     main {
@@ -22,5 +32,11 @@
         height: 100%;
         flex-grow: 1;
         overflow-y: auto;
+    }
+
+    footer {
+        padding: 4px;
+        text-align: center;
+        background-color: var(--mdc-theme-surface, #212125);
     }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,13 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-netlify";
 
 const config = {
-  preprocess: preprocess(),
+    preprocess: preprocess(),
 
-  kit: {
-    adapter: adapter(),
-    target: '#svelte',
-  },
+    kit: {
+        adapter: adapter(),
+        target: "#svelte",
+    },
 };
 
 export default config;
