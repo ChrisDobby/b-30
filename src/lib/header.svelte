56124167cfa385d8d97b33811f862ea19d5abb1e
Chris Dobson <chrisdobby.dev@gmail.com>
2021-11-14 17:48:34 +0000

    created header component

---
+++ src/lib/header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
+    import IconButton from "@smui/icon-button";
+    import Button from "@smui/button";
+</script>
+
+<TopAppBar variant="static" color={"primary"}>
+    <Row>
+        <Section>
+            <IconButton class="material-icons">menu</IconButton>
+            <Title>Breaking 30</Title>
+        </Section>
+        <Section align="end">
+            <Button href="/activities" outlined color="primary">Login</Button>
+        </Section>
+    </Row>
+</TopAppBar>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,14 @@
+<script context="module">
+    /** @type {import('@sveltejs/kit').Load} */
+    export async function load({ session }) {
+        return { props: { user: session.user } };
+    }
+</script>
+
+<script lang="ts">
+    import "../app.scss";
+</script>
+
 <div class="coming-soon">
     <h4>Coming soon!</h4>
 </div>
