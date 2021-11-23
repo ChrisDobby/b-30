2ce43d447b2b5b7bb2ad0a9b5f82618d46b5cfea
Chris Dobson <chrisdobby.dev@gmail.com>
2021-11-23 14:28:55 +0000

    add app bar menu

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -2,13 +2,40 @@
     import TopAppBar, { Row, Section, Title } from "@smui/top-app-bar";
     import IconButton from "@smui/icon-button";
     import Button from "@smui/button";
+    import Menu, { MenuComponentDev } from "@smui/menu";
+    import List, { Item, Separator, Text } from "@smui/list";
     import { session } from "$app/stores";
+
+    let menu: MenuComponentDev;
 </script>
 
 <TopAppBar variant="static" color={"primary"}>
     <Row>
         <Section>
-            <IconButton class="material-icons">menu</IconButton>
+            <IconButton on:click={() => menu.setOpen(true)} class="material-icons">menu</IconButton>
+            <Menu bind:this={menu}>
+                <List>
+                    {#if $session.user}
+                        <Item>
+                            <Button href="/set-times">
+                                <Text>Set paces</Text>
+                            </Button>
+                        </Item>
+                        <Separator />
+                        <Item>
+                            <Button href="/api/logout" rel="external">
+                                <Text>Logout</Text>
+                            </Button>
+                        </Item>
+                    {:else}
+                        <Item>
+                            <Button href="/activities">
+                                <Text>Login</Text>
+                            </Button>
+                        </Item>
+                    {/if}
+                </List>
+            </Menu>
             <Title>Breaking 30</Title>
         </Section>
         <Section align="end">
