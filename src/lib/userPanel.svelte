fd69bd4bdc4028be05e8a4d0713176b2805d576c
chrisdobby <dobby@chrisjdobson.co.uk>
2021-11-25 15:58:54 +0000

    add option to set times from user panel

--- src/lib/userPanel.svelte
+++ src/lib/userPanel.svelte
@@ -3,6 +3,8 @@
     import { session } from "$app/stores";
     import { timeFromSeconds, calculateSpeedAndPace } from "$lib/utils";
     import PaceGrid from "$lib/paceGrid.svelte";
+    import Button from "@smui/button";
+    import SetTimesForm from "$lib/setTimesForm.svelte";
 
     const getSpeedAndPace = calculateSpeedAndPace($session.measurementPreference);
 
@@ -11,6 +13,10 @@
     session.subscribe(update => {
         times = update.times;
     });
+
+    let settingTimes = false;
+
+    const onSettingComplete = () => (settingTimes = false);
 </script>
 
 {#if user}
@@ -19,7 +25,7 @@
         <div class="header">
             <div class="user-name">{`${user.firstName} ${user.lastName}`}</div>
         </div>
-        <div class="times-header">
+        <div class={`times-header ${settingTimes ? "disabled" : ""}`}>
             {#if times}
                 <div class="five-k-description">
                     {`Date 5k time: ${timeFromSeconds(times.date5k)} at ${getSpeedAndPace(5000 / times.date5k)[1]}`}
@@ -29,9 +35,15 @@
             {/if}
         </div>
         {#if times}
-            <div class="times-detail">
+            <div class={`times-detail ${settingTimes ? "disabled" : ""}`}>
                 <PaceGrid {times} measurementPreference={$session.measurementPreference} />
             </div>
+        {/if}
+        <Button disabled={settingTimes} on:click={() => (settingTimes = true)}
+            >{times ? "Update paces" : "Set paces"}</Button
+        >
+        {#if settingTimes}
+            <SetTimesForm onCancel={onSettingComplete} onChange={onSettingComplete} />
         {/if}
     </div>
 {/if}
@@ -53,5 +65,10 @@
         height: 5em;
         border-radius: 50%;
         margin-right: 1em;
+    }
+
+    .times-detail.disabled,
+    .times-header.disabled {
+        opacity: 0.2;
     }
 </style>
